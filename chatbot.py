097e809c47bf60187be067713631fa72afc01e79
Just R <remixingmagelang@gmail.com>
2024-10-02 04:30:10 +0700

    update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -154,7 +154,7 @@ async def handle_message(client, message):
     try:
         result = my_api.ChatBot(f"<blockquote>message</blockquote>")
         logger.get_logger(__name__).info("Mengirim output besar ke pengguna")
-        await Handler().sendLongPres(message, result, parse_mode='html')
+        await Handler().sendLongPres(message, result)
     except FloodWait as e:
         await asyncio.sleep(e.x)  # Wait for the required time before retrying
     except Exception as e:
