84d1cec566d10948a2a95626b676b36d63f70071
Just R <remixingmagelang@gmail.com>
2024-10-02 17:55:54 +0700

    update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -128,7 +128,7 @@ async def handle_message(client, message):
                 f"<blockquote>🔄 Pembaruan berhasil! Bot telah diperbarui. 🚀</blockquote>"
             )
 
-            subprocess.run(["bash start"])
+            subprocess.run(["start"], shell=True)
 
         except Exception as e:
             await message.reply(f"Terjadi kesalahan saat memperbarui: {e} ⚠️")
@@ -152,9 +152,9 @@ async def handle_message(client, message):
     await client.send_chat_action(chat_id=message.chat.id, action=ChatAction.TYPING)
 
     try:
-        result = my_api.ChatBot(message)
+        result = my_api.ChatBot(f"<blockquote>message</blockquote>")
         logger.get_logger(__name__).info("Mengirim output besar ke pengguna")
-        await Handler().sendLongPres(result)
+        await Handler().sendLongPres(message, result)
     except FloodWait as e:
         await asyncio.sleep(e.x)  # Wait for the required time before retrying
     except Exception as e:
